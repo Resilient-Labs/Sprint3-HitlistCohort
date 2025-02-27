b905d7b91f7451769b26c0e6ca7d76d397f4583e
Niya Nixon <162916832+niyanixon@users.noreply.github.com>
2025-02-27 14:17:46 -0500

    Api/add company delete company (#45)
    * add company and delete
    
    * fixed api add company and delete
    
    * fixed api add company and delete #8
    
    ---------
    
    Co-authored-by: Zaria Hallager <120356303+ZariaHallager@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,55 @@ require('dotenv').config({ path: `${__dirname}/../.env` })
 const express = require('express')
 const app = express()
 const main = require('./mongoose')
-const morgan = require("morgan");
-const middleware = require('./utils/middleware')
-const cors = require("cors")
-const PORT = 3001
+const Company = require('./models/company.schema');
 
-// app.use(main)
-app.use(cors());
+
+// app.get('/', (request, response) => {
+//   response.send('<h1>Hello World!</h1>')
+// })
+
+//***** ADD COMPANY ***** 
+app.post('/companies', async (req,res) => {
+  try { 
+    const { name, status, applicationUrl, notes, pointOfContact } = req.body;
+
+    const newCompany = new Company ({
+      name,
+      status,
+      applicationUrl,
+      notes,
+      pointOfContact,
+    })
+    await newCompany.save();
+
+    console.log('Company saved:', newCompany)
+    res.status(201).json(newCompany)
+  }catch (error){
+    console.error("Error saving company", error)
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+})
+
+//***** DELETE COMPANY *****
+app.delete('/companies/:id', async (req, res) => {
+  try {
+    const deletedCompany = await Company.findByIdAndDelete(req.params.id);
+
+    if (!deletedCompany) {
+      return res.status(404).json({ message: "Company not found" });
+    }
+
+    res.json({ message: "Company Deleted", deletedCompany });
+  } catch (error) {
+    console.error("Error deleting company:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
 app.use(express.static('dist'))
-app.use(express.json())
-app.use(morgan("dev"));
-
-
-app.use(middleware.unknownEndpoint);
-
+app.use(express.json());
+app.use(main)
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })

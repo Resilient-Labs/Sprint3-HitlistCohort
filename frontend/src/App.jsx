3623874e89440483e9315cbd55e1881a747420cc
ericamendez <ecmendez25@gmail.com>
2025-02-27 14:29:03 +0100

    just a quick cleanup, formated the files in both backend folder and frontend folder running [npm run format], and added package-lock to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
 
 function App() {
-
-  return (
-    <>
-     <h1>Job Hitlist</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>Job Hitlist</h1>
+        </>
+    )
 }
 
 export default App

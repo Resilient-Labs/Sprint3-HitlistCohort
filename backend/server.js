81a6cc04d59c51f6db4fa1ca7fefb57915cb3934
abdiladifgurhan <abdiladifgurhan@gmail.com>
2025-02-27 13:25:50 -0500

    Added cofiguration for cors, and added middleware for http request logging,and added utils folder

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,20 @@ require('dotenv').config({ path: `${__dirname}/../.env` })
 const express = require('express')
 const app = express()
 const main = require('./mongoose')
-
-// app.get('/', (request, response) => {
-//   response.send('<h1>Hello World!</h1>')
-// })
-
-app.use(express.static('dist'))
-app.use(main)
-
+const morgan = require("morgan");
+const middleware = require('./utils/middleware')
+const cors = require("cors")
 const PORT = 3001
+
+// app.use(main)
+app.use(cors());
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(morgan("dev"));
+
+
+app.use(middleware.unknownEndpoint);
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })

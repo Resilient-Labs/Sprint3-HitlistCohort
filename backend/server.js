fa60c5c80f36612e5314ab02712bd792a0658e49
Niya Nixon <niyanixon06@gmail.com>
2025-02-27 11:12:28 -0500

    add company and delete

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,30 @@ const app = express()
 //   response.send('<h1>Hello World!</h1>')
 // })
 
+//***** ADD COMPANY ***** */
+app.post('/companies',(req,res) => {
+  const { name, status, applicationUrl, notes, pointOfContact } = req.body;
+
+  const newCompany = {
+    name,
+    status,
+    applicationUrl,
+    notes: notes || '',
+    pointOfContact,
+  }
+  companies.push(newCompany)
+
+  console.log('Company saved:', newCompany)
+  res.status(201).json(newCompany)
+})
+
+//***** DELETE COMPANY *****
+app.delete('/companies/:id', (req,res) => {
+    companies = companies.filter(c => c.id !== parseInt(req.params.id))
+    res.json({message: 'Company Deleted'})
+})
+
+
 app.use(express.static('dist'))
 
 const PORT = 3001

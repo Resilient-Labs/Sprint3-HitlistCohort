f749cf093dd8fd6bfbd28f831d169d4fc623bd02
ericamendez <ecmendez25@gmail.com>
2025-02-27 14:37:05 +0100

    moving elint to frontend folder

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,8 +1,11 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import react from 'eslint-plugin-react'
+import prettier from 'eslint-config-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import unusedImports from 'eslint-plugin-unused-imports';
+import importPlugin from 'eslint-plugin-import';
 
 export default [
     { ignores: ['dist'] },
@@ -10,18 +13,20 @@ export default [
         files: ['**/*.{js,jsx}'],
         languageOptions: {
             ecmaVersion: 2020,
-            globals: globals.browser,
+            globals: { ...globals.browser, ...globals.node },
             parserOptions: {
                 ecmaVersion: 'latest',
                 ecmaFeatures: { jsx: true },
                 sourceType: 'module',
             },
         },
-        settings: { react: { version: '18.3' } },
+        settings: { react: { version: 'detect' } },
         plugins: {
             react,
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
+            'unused-imports': unusedImports,
+            'import': importPlugin
         },
         rules: {
             ...js.configs.recommended.rules,
@@ -29,10 +34,13 @@ export default [
             ...react.configs['jsx-runtime'].rules,
             ...reactHooks.configs.recommended.rules,
             'react/jsx-no-target-blank': 'off',
+            'react/prop-types': 'off',
             'react-refresh/only-export-components': [
                 'warn',
                 { allowConstantExport: true },
             ],
+            'unused-imports/no-unused-imports': 'error',
         },
     },
+    prettier,
 ]

875b4f7127e62dcabb75acde1dc3b9d66989734b
Sxnny-s <ethsok@protonmail.com>
2025-02-28 11:27:39 -0500

    Add GET and PUT API routes for company info, enable JSON parsing

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,54 @@ const express = require('express')
 const app = express()
 const main = require('./mongoose')
 const Company = require('./models/company.schema');
+app.use(express.json()); // Enables JSON parsing
+// app.use(express.urlencoded({ extended: true })); // Parses URL-encoded data
+
 
 
 // app.get('/', (request, response) => {
 //   response.send('<h1>Hello World!</h1>')
 // })
+
+
+
+//***** GET COMPANY INFO*****
+app.get('/companies', async (req, res) => {
+  try {
+    await Company.find({}).then(company => {
+      res.json(company)
+    })
+  } catch (error) {
+    console.error("Error getting companies", error)
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+})
+
+
+//***** Edit company info *****
+
+app.put('/companies/:id', async (req,res) => {
+  try {
+      const body = req.body
+      const companyInfo = {
+        name: body.name,
+        status: body.status,
+        applicationUrl: body.applicationUrl,
+        notes: body.notes,
+        pointOfContacts: body.pointOfContacts
+      }
+      const ID = req.params.id
+      Company.findByIdAndUpdate(ID,companyInfo,{new: true})
+        .then(updatedCompnayInfo => {
+          res.json(updatedCompnayInfo)
+        })
+
+  } catch (error) { 
+    console.error("Error Editing compnay info")
+    res.status(500).json({error: "Internal Server Error"})
+  }
+})
+
 
 //***** ADD COMPANY ***** 
 app.post('/companies', async (req,res) => {
@@ -48,6 +91,10 @@ app.delete('/companies/:id', async (req, res) => {
 });
 
 
+
+
+
+const PORT = process.env.PORT || 3000;
 app.use(express.static('dist'))
 app.use(express.json());
 app.use(main)

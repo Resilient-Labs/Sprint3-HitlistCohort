ea7d38dcc347c96c20c347289361c35161c1e287
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-03-02 16:24:15 -0500

    merged main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,14 @@ import CompanyList from './components/CompanyList'
 import ContactList from './components/ContactList'
 import CompanyForm from './components/CompanyForm'
 import { useEffect, useState } from 'react'
-import { fetchCompanies } from './services/Company' 
+import companyService from './services/Company' 
 
 function App() {
     const [data, setData] = useState([])
 
     useEffect(() => {
         const getCompanies = async () => {
-            const response = await fetchCompanies()
+            const response = await companyService.getAll()
             setData(response)
         }
         getCompanies()
--- frontend/src/services/company.js
+++ frontend/src/services/company.js
@@ -1,13 +1,47 @@
 import axios from 'axios'
+const baseUrl = `http://localhost:3001/companies`
 
-const API_URL = 'http://localhost:3001/companies'
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request
+        .then((response) => response.data)
+        .catch((error) => {
+            console.error('GET error', error)
+        })
+}
 
-export const fetchCompanies = async () => {
-  try{
-    const response = await axios.get(API_URL)
-    return Array.isArray(response.data) ? response.data : []
-  } catch (err) {
-    console.error("Error fetching companies:", err)
-    return []
-  }
+const create = (newObject) => {
+    const request = axios.post(baseUrl, newObject)
+    return request
+        .then((response) => response.data)
+        .catch((error) => {
+            console.error('POST error', error)
+        })
+}
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request
+        .then((response) => response.data)
+        .catch((error) => {
+            console.error('PUT error', error)
+        })
+}
+
+const remove = (id) => {
+    axios
+        .delete(`${baseUrl}/${id}`)
+        .then((response) => {
+            return response.data
+        })
+        .catch((error) => {
+            console.error('DELETE error', error)
+        })
+}
+
+export default {
+    getAll,
+    create,
+    update,
+    remove,
 }

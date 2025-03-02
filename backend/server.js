8d5d52c950cc44c7de1435b915185d6b7aaccdb6
Sebastian Vivas <seb.vivas.dev@gmail.com>
2025-03-02 13:17:27 -0500

    ISSUE-21

--- backend/server.js
+++ backend/server.js
@@ -6,130 +6,139 @@ const Company = require('./models/company.schema')
 const User = require('./models/user.schema')
 const jwt = require('jsonwebtoken')
 const cors = require('cors')
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
-app.use(express.json());
-app.use(cors({ origin: `http://localhost:${PORT}`, optionsSuccessStatus: 200 }))
+app.use(express.json())
+app.use(cors({ origin: `http://localhost:5173`, optionsSuccessStatus: 200 }))
 
 app.post('/sign-up', async (req, res) => {
-  try {
-      const { email, password, username } = req.body;
+    try {
+        const { email, password, username } = req.body
 
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-          return res.status(400).json({ message: 'Email already in use' });
-      }
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email already in use' })
+        }
 
-      const user = new User({ email, password, username });
+        const user = new User({ email, password, username })
 
-      const token = jwt.sign({ email, password, username }, 'xxx-xxx', { expiresIn: '1h' });
-
-      await user.save();
-
-      res.status(201).json({ user, message: 'User registered successfully', token });
-  } catch (error) {
-      res.status(500).json({ message: 'Server error', error: error.message });
-  }
-});
-
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-      const user = await User.findOne({ email });
-      console.log(user)
-
-      if (!user) {
-          return res.status(400).json({ message: 'Invalid email or password' });
-      }
-
-      const isMatch = await user.comparePassword(password);
-
-      if (!isMatch) {
-          return res.status(400).json({ message: 'Invalid email or password' });
-      }
-
-      const token = jwt.sign({ email, password }, "xxx-xxx", { expiresIn: '1h' });
-
-      res.json({message: 'Login successful', token, user });
-  } catch (error) {
-      res.status(500).json({ message: 'Server error', error: error.message });
-  }
-});
-
-//***** GET COMPANY INFO*****
-app.get('/companies', async (req, res) => {
-  try {
-    await Company.find({}).then(company => {
-      res.json(company)
-    })
-  } catch (error) {
-    console.error("Error getting companies", error)
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-})
-
-
-//***** EDIT COMPANY INFO *****
-app.put('/companies/:id', async (req,res) => {
-  try {
-      const body = req.body
-      const companyInfo = {
-        name: body.name,
-        status: body.status,
-        applicationUrl: body.applicationUrl,
-        notes: body.notes,
-        pointOfContacts: body.pointOfContacts
-      }
-      const ID = req.params.id
-      Company.findByIdAndUpdate(ID,companyInfo,{new: true})
-        .then(updatedCompnayInfo => {
-          res.json(updatedCompnayInfo)
+        const token = jwt.sign({ email, password, username }, 'xxx-xxx', {
+            expiresIn: '1h',
         })
 
-  } catch (error) { 
-    console.error("Error Editing compnay info")
-    res.status(500).json({error: "Internal Server Error"})
-  }
+        await user.save()
+
+        res.status(201).json({
+            user,
+            message: 'User registered successfully',
+            token,
+        })
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message })
+    }
+})
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.findOne({ email })
+        console.log(user)
+
+        if (!user) {
+            return res
+                .status(400)
+                .json({ message: 'Invalid email or password' })
+        }
+
+        const isMatch = await user.comparePassword(password)
+
+        if (!isMatch) {
+            return res
+                .status(400)
+                .json({ message: 'Invalid email or password' })
+        }
+
+        const token = jwt.sign({ email, password }, 'xxx-xxx', {
+            expiresIn: '1h',
+        })
+
+        res.json({ message: 'Login successful', token, user })
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message })
+    }
 })
 
 //***** GET COMPANY INFO*****
 app.get('/companies', async (req, res) => {
-  try {
-    await Company.find({}).then(company => {
-      res.json(company)
-    })
-  } catch (error) {
-    console.error("Error getting companies", error)
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+    try {
+        await Company.find({}).then((company) => {
+            res.json(company)
+        })
+    } catch (error) {
+        console.error('Error getting companies', error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
 })
 
+//***** EDIT COMPANY INFO *****
+app.put('/companies/:id', async (req, res) => {
+    try {
+        const body = req.body
+        const companyInfo = {
+            name: body.name,
+            status: body.status,
+            applicationUrl: body.applicationUrl,
+            notes: body.notes,
+            pointOfContacts: body.pointOfContacts,
+        }
+        const ID = req.params.id
+        Company.findByIdAndUpdate(ID, companyInfo, { new: true }).then(
+            (updatedCompnayInfo) => {
+                res.json(updatedCompnayInfo)
+            },
+        )
+    } catch (error) {
+        console.error('Error Editing compnay info')
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+
+//***** GET COMPANY INFO*****
+app.get('/companies', async (req, res) => {
+    try {
+        await Company.find({}).then((company) => {
+            res.json(company)
+        })
+    } catch (error) {
+        console.error('Error getting companies', error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
 
 //***** Edit company info *****
 
-app.put('/companies/:id', async (req,res) => {
-  try {
-      const body = req.body
-      const companyInfo = {
-        name: body.name,
-        status: body.status,
-        applicationUrl: body.applicationUrl,
-        notes: body.notes,
-        pointOfContacts: body.pointOfContacts
-      }
-      const ID = req.params.id
-      Company.findByIdAndUpdate(ID,companyInfo,{new: true})
-        .then(updatedCompnayInfo => {
-          res.json(updatedCompnayInfo)
-        })
-
-  } catch (error) { 
-    console.error("Error Editing compnay info")
-    res.status(500).json({error: "Internal Server Error"})
-  }
+app.put('/companies/:id', async (req, res) => {
+    try {
+        const body = req.body
+        const companyInfo = {
+            name: body.name,
+            status: body.status,
+            applicationUrl: body.applicationUrl,
+            notes: body.notes,
+            pointOfContacts: body.pointOfContacts,
+        }
+        const ID = req.params.id
+        Company.findByIdAndUpdate(ID, companyInfo, { new: true }).then(
+            (updatedCompnayInfo) => {
+                res.json(updatedCompnayInfo)
+            },
+        )
+    } catch (error) {
+        console.error('Error Editing compnay info')
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
 })
-
 
 //***** ADD COMPANY *****
 app.post('/companies', async (req, res) => {
@@ -171,18 +180,22 @@ app.delete('/companies/:id', async (req, res) => {
 
 //***** GET ALL POINTS OF CONTACTS *****
 app.get('/all-contacts', async (req, res) => {
-  try {
-    const companiesPoc = await Company.find({}, 'pointOfContacts -_id');
-    const allContacts = companiesPoc
-      .map(company => Array.isArray(company.pointOfContacts) ? company.pointOfContacts : [])
-      .flat()
+    try {
+        const companiesPoc = await Company.find({}, 'pointOfContacts -_id')
+        const allContacts = companiesPoc
+            .map((company) =>
+                Array.isArray(company.pointOfContacts)
+                    ? company.pointOfContacts
+                    : [],
+            )
+            .flat()
 
-    res.json({ allContacts });
-  } catch (error) {
-    console.error("Error fetching point of contactS:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+        res.json({ allContacts })
+    } catch (error) {
+        console.error('Error fetching point of contactS:', error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
 
 app.use(express.static('dist'))
 app.use(main)

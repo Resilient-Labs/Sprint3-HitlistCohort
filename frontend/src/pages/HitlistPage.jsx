089846ea271830696b805d2632a75db5ed5dd84d
Danny Garcia <dannygarciaDEV@gmail.com>
2025-03-02 17:27:37 -0500

    adding companylist component to hitlist page

--- frontend/src/pages/HitlistPage.jsx
+++ frontend/src/pages/HitlistPage.jsx
@@ -1,10 +1,13 @@
+import CompanyList from '../components/CompanyList'
 import Navbar from '../components/Navbar'
+
 
 const HitlistPage = () => {
     return (
         <>
             <Navbar />
             <div>Hitlist Page</div>
+            <CompanyList/>
         </>
     )
 }

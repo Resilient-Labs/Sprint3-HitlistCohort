4eec78b63d18068616bef80be2d306876e23b314
Roger Cruz <roger.david.cruz@gmail.com>
2025-03-02 10:41:56 -0500

    modified column header to sort

--- frontend/src/components/CompanyList.jsx
+++ frontend/src/components/CompanyList.jsx
@@ -1,19 +1,41 @@
+import React, { useState, useEffect } from "react";
+import SortColumn from "./SortColumn";
 import './CompanyList.css'
 
 const CompanyList = ({ data }) => {
+    const [sortedData, setSortedData] = useState([...data]);
+
+    useEffect(() => {
+        setSortedData([...data]);
+    }, [data]);
+
+    const handleSort = (columnKey, order) => {
+        const sorted = [...data].sort((a,b) => {
+            let valueA = a[columnKey]?.toLowerCase() || "";
+            let valueB = b[columnKey]?.toLowerCase() || "";
+            return order === "asc" ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA);
+        });
+
+    setSortedData(sorted);
+    };
+
     return (
         <div>
             <h2>Companies</h2>
             <table>
-                <tbody>
+                <thead>
                     <tr className="header-row">
-                        <th>Name</th>
+                        <th>
+                            <SortColumn label="Name" columnKey="name" onSortChange={handleSort}></SortColumn>
+                        </th>
                         <th>Status</th>
                         <th>Application URL</th>
                         <th>Notes</th>
                         <th>Point of Contacts</th>
                     </tr>
-                    {data.map((company, index) => (
+                </thead>
+                <tbody>
+                    {sortedData.map((company, index) => (
                         <tr key={index} className="data-rows">
                             <td>{company.name}</td>
                             <td>{company.status}</td>

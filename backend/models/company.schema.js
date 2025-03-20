d50566110cf337ee6770c71e149cc74dab4a8faf
Charles De Farias <cddefari@gmail.com>
2025-03-20 01:32:31 -0400

    removed work from other branch

--- backend/models/company.schema.js
+++ backend/models/company.schema.js
@@ -5,7 +5,7 @@ const companySchema = new mongoose.Schema({
     status: String,
     applicationUrl: String,
     notes: String,
-    pointOfContacts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Contact' }],
+    pointOfContacts: [],
 })
 
 const CompanyModel = mongoose.model('Company', companySchema)

4abea1ce6fef10e28ed3e60a8bab7dcd6aef6ad6
Raey-Abera <ab.raey@gmail.com>
2025-03-21 18:46:32 -0400

    adding API routes for contacts

---
+++ backend/routes/contacts.js
@@ -0,0 +1,67 @@
+const contactsRouter = require('express').Router()
+const Contact = require('../models/contact.schema');
+
+// ADD A CONTACT TO A COMPANY
+contactsRouter.post('/contacts', async (req, res) => {
+    try {
+        const { name, email, phone, companyId } = req.body;
+
+        if(!companyId) {
+            return res.status(400).json({
+            message: 'Company ID is required'
+            });
+        }
+
+        const newContact = new Contact({ name, email, phone, companyId});
+        await newContact.save();
+
+        res.status(201).json(newContact);
+    } catch (error) {
+        console.error('Error adding contact:', error);
+        res.status(500).json({error: 'Internal Server Error '});
+    }
+});
+
+// GET CONTACT DETAILS
+contactsRouter.get('/contacts/:id', async (req, res) => {
+    try {
+        const contact = await Contact.findById(req.params.id);
+        if (!contact) {
+            return res.status(404).json({ message: 'Contact not found' });
+        }
+        res.json(contact);
+    } catch (error) {
+        console.error('Error fetching contact:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// UPDATE CONTACT DETAILS
+contactsRouter.put('/contacts/:id', async (req, res) => {
+    try {
+        const updatedContact = await Contact.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedContact) {
+            return res.status(404).json({ message: 'Contact not found' });
+        }
+        res.json(updatedContact);
+    } catch (error) {
+        console.error('Error updating contact:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// DELETE CONTACTS 
+contactsRouter.delete('/contacts/:id', async (req, res) => {
+    try {
+        const deletedContact = await Contact.findByIdAndDelete(req.params.id);
+        if (!deletedContact) {
+            return res.status(404).json({ message: 'Contact not found' });
+        }
+        res.json({ message: 'Contact deleted', deletedContact });
+    } catch (error) {
+        console.error('Error deleting contact:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+module.exports = contactsRouter;

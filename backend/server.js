bf5a7576e7c86ec57e3f81fda202c9610678b132
sxnny-s <115429120+Sxnny-s@users.noreply.github.com>
2025-03-26 18:43:34 -0400

    Removed server export because it was not needed for the test to run.

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,12 @@ app.use('/', authenticationRouter)
 //must go after routes or it will block them
 app.use(main)
 
-let server;
+
+
 if (process.env.NODE_ENV !== 'test') {
     server = app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
 }
 
-module.exports = {app ,server}
+module.exports = {app}
--- backend/tests/creating-company-test.js
+++ backend/tests/creating-company-test.js
@@ -1,7 +1,7 @@
 const {test, after} = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
-const {app, server} = require('../server' )
+const {app} = require('../server' )
 const  assert  = require('assert')
 
 const api = supertest(app)
@@ -43,9 +43,4 @@ test.only('Company was added successfully', async () => {
 after(async () => {
     console.log("Closing database connection and server...")
     await mongoose.connection.close()
-    if (server) {
-        server.close(() => {
-            console.log("Server closed.");
-        });
-    }
 })

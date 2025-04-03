b21b597206a4d6f3ebaaa366b5258632e6f536f5
Kaia Walters <kaia.cwalters@gmail.com>
2025-04-03 11:47:21 -0400

    Revert "Revert "User sign in#119 (#143)" (#145)"
    This reverts commit 89785c635d6a957f2f01e21a49f7c069d1d9ecf8.

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -5,11 +5,15 @@ const AuthContext = createContext()
 const AuthProvider = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false)
     const [loading, setLoading] = useState(true)
+    const [username, setUsername] = useState(null)
 
     useEffect(() => {
         const token = localStorage.getItem('jwtToken')
+        const storedUsername = localStorage.getItem('userId')
 
         setIsAuthenticated(!!token) // Ensure boolean value
+        setUsername(storedUsername)
+
         setLoading(false)
     }, [])
 
@@ -36,6 +40,8 @@ const AuthProvider = ({ children }) => {
             localStorage.setItem('userId', username)
 
             setIsAuthenticated(true)
+            setUsername(username)
+
             setTimeout(() => (window.location.href = '/'), 1000)
         } catch (error) {
             console.error('Login error:', error)
@@ -44,7 +50,7 @@ const AuthProvider = ({ children }) => {
     }
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+        <AuthContext.Provider value={{ isAuthenticated, login, logout, username }}>
             {children}
         </AuthContext.Provider>
     )

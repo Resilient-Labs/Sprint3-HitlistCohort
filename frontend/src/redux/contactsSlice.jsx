d9ccf193d32e6633743415847aa056a3e6f118d4
KaiaWalters <kaia.cwalters@gmail.com>
2025-04-14 23:06:54 -0400

    creates contacts slice

---
+++ frontend/src/redux/contactsSlice.jsx
@@ -0,0 +1,56 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const contactsSlice = createSlice({
+    name: 'contacts',
+    initialState: {
+        contacts:[]
+    },
+    reducers: {
+        setInitialState: (state, action) => {
+            return {
+                ...state, 
+                contacts : action.payload
+            }
+        }, 
+        addCompany: (state, action) => {
+                const newCompany = action.payload
+                return {
+                    ...state,
+                    contacts : [...state.contacts , newCompany],
+                }
+            }, 
+        deleteCompanyById: (state, action) => {
+                const id = action.payload
+                return {
+                    ...state,
+                    contacts : state.contacts .filter(
+                        (company) => company._id !== id
+                    ),
+                }
+            }, 
+        sortcontacts : (state, action) => {
+                const { columnKey, order } = action.payload
+                return {
+                    ...state,
+                    contacts: state.contacts .sort((a, b) => {
+                        let valueA = a[columnKey]?.toLowerCase() || ''
+                        let valueB = b[columnKey]?.toLowerCase() || ''
+                        return order === 'asc'
+                            ? valueA.localeCompare(valueB)
+                            : valueB.localeCompare(valueA)
+                    }),
+                }
+            }
+    }
+  })
+  
+
+export const {
+    setInitialState,
+    addContact,
+    deleteContactById,
+    sortcontacts ,
+  } = contactsSlice.actions
+  
+export default contactsSlice.reducer
+  
--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import companyReducer from './companySlice'
+import contactsReducer from './contactsSlice'
 
 /* 
     TODO: No slices have been created yet. The commented out slices are just here as an example of next steps.
@@ -7,7 +8,8 @@ import companyReducer from './companySlice'
 
 const store = configureStore({
     reducer: {
-        companies: companyReducer
+        companies: companyReducer,
+        contacts: contactsReducer
     },
 })
 
